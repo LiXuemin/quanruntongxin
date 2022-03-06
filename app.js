6cc1d2eeb08f826a3856a382715e1b36ca7177c4
lixuemin <lxmheida@gmail.com>
2022-03-06 14:18:40 +0800

    登录，并存储用户信息到本地globalData

--- app.js
+++ app.js
@@ -9,46 +9,28 @@ App({
       env: 'dev-8grcb747413937fe',
       traceUser: true
     })
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res)
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+  },
+  _getUserInfo: function () {
+    var userInfoStorage = wx.getStorageSync('user');
+    if(!userInfoStorage) {
+      var that = this;
+      wx.login({
+        success: function() {
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log("获取用户信息", this.globalData.userInfo)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+            success: function(res) {
+              that.globalData.userInfo = res.userInfo
+              wx.setStorageSync('user', res.userInfo)
+            },
+            fail: function() {
+              console.log(res)
             }
           })
         }
-      }
-    })
-    // 获取系统状态栏信息
-    wx.getSystemInfo({
-      success: e => {
-        this.globalData.StatusBar = e.statusBarHeight;
-        let capsule = wx.getMenuButtonBoundingClientRect();
-        if (capsule) {
-          this.globalData.Custom = capsule;
-          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
-        } else {
-          this.globalData.CustomBar = e.statusBarHeight + 50;
-        }
-      }
-    })
+      })
+    }else {
+      this.globalData.userInfo = userInfoStorage;
+    }
+
   },
   globalData: {
     userInfo: null
